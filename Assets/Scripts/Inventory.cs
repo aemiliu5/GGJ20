4b22e9d897bdfbd85d9a940fe22f962c15560e7e
Aemilius <seimenace@gmail.com>
2020-02-01 01:08:37 +0200

    Lots of stuff happened (axe animation)

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    [Header("Materials")]
+    public int wood;
+    public int rope;
+    public int sail;
+
+    [Header("Game Objects")]
+    public GameObject axe;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Axe"))
+        {
+            axe.SetActive(true);
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        // Axe Attack
+        if (axe.activeSelf)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                axe.GetComponent<Animator>().Play("Axe");
+            }
+            else
+            {
+                axe.GetComponent<Animator>().Play("Axe_Idle");
+            }
+        }
+    }
+}
