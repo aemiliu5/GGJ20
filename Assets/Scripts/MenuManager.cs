70b8f495258616b8ba88d8deb7c3c9c4dc1a54cc
Aemilius <seimenace@gmail.com>
2020-02-02 03:58:29 +0200

    Menu

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject credits;
+    public GameObject options;
+    public Resolution[] r;
+    public int currentRes;
+    public bool fullScreen;
+
+    private void Start()
+    {
+        r = Screen.resolutions;
+    }
+
+    public void BackToMenu()
+    {
+        mainMenu.SetActive(true);
+        credits.SetActive(false);
+        options.SetActive(false);
+    }
+
+    public void Credits()
+    {
+        mainMenu.SetActive(false);
+        credits.SetActive(true);
+        options.SetActive(false);
+    }
+
+    public void Options()
+    {
+        mainMenu.SetActive(false);
+        credits.SetActive(false);
+        options.SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void NextRes()
+    {
+        if(currentRes+1 >= r.Length)
+        {
+            currentRes = 0;
+        }
+        else
+        {
+            currentRes++;
+        }
+    }
+
+    public void PrevRes()
+    {
+        if (currentRes-1 < 0)
+        {
+            currentRes = r.Length-1;
+        }
+        else
+        {
+            currentRes++;
+        }
+    }
+
+    public void Update()
+    {
+        GameObject.Find("Resolution").GetComponent<Text>().text = "Resolution: " + r[currentRes].ToString();
+    }
+
+    public void FullscreenTrue()
+    {
+        fullScreen = true;
+        GameObject.Find("Fullscreen").GetComponent<Text>().text = "Fullscreen: On";
+    }
+
+    public void FullscreenFalse()
+    {
+        fullScreen = false;
+        GameObject.Find("Fullscreen").GetComponent<Text>().text = "Fullscreen: Off";
+    }
+
+    public void vSyncOn()
+    {
+        QualitySettings.vSyncCount = 1;
+        GameObject.Find("VSync").GetComponent<Text>().text = "VSync: On";
+    }
+
+    public void vSyncOff()
+    {
+        QualitySettings.vSyncCount = 0;
+        GameObject.Find("VSync").GetComponent<Text>().text = "VSync: Off";
+    }
+
+    public void ApplySettings()
+    {
+        Screen.SetResolution(r[currentRes].width, r[currentRes].height, fullScreen);
+    }
+}
