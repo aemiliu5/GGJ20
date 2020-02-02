6f018c91db12f84d31e3ae0dc267b83fb583ac7b
Nickzouk <Nickzouk2000@gmail.com>
2020-02-02 12:22:24 +0200

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     public float sprintMultiplier;
     public bool isGrounded;
     public float footstepTimer = 0f;
+    public float raycastHeight;
 
     [Header("Audio Clips")]
     public AudioClip sandClip;
@@ -42,12 +43,12 @@ public class PlayerController : MonoBehaviour
 
         // Jump & Grounded Check
         Ray r = new Ray(transform.position - new Vector3(0, 2.2f, 0), Vector3.down);
-        Debug.DrawRay(r.origin, r.direction * 1.6f);
-        isGrounded = Physics.Raycast(r, 1.6f, 1 << 9);
+        Debug.DrawRay(r.origin, r.direction * raycastHeight);
+        isGrounded = Physics.Raycast(r, raycastHeight, 1 << 9);
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            GetComponent<Rigidbody>().AddForce(0, jumpHeight * Time.deltaTime, 0);
+            GetComponent<Rigidbody>().AddForce(0, jumpHeight  , 0);
             voice.clip = jumpClip;
             voice.Play();
         }
